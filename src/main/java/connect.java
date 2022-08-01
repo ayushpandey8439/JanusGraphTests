d3f3863bbccf30ac955040725ae363f6e6d78ea0
Ayush Pandey <ayush.pandey@lip6.fr>
2022-08-01 16:49:58 +0200

    Add logic for updating path labels in JanusGraph Vertices

--- src/main/java/connect.java
+++ src/main/java/connect.java
@@ -1,30 +1,38 @@
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
-import org.apache.tinkerpop.gremlin.structure.Edge;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.janusgraph.core.JanusGraph;
 import org.janusgraph.core.JanusGraphFactory;
-import org.janusgraph.core.attribute.Geo;
-import org.janusgraph.core.attribute.Geoshape;
-import org.janusgraph.example.GraphOfTheGodsFactory;
+import org.janusgraph.core.JanusGraphVertex;
+
 
 
 import java.util.List;
 
 public class connect {
-
     public static void main(String[] args) throws Exception {
         JanusGraph graph = JanusGraphFactory.open("src/conf/remote.properties");
         System.out.println(graph.toString());
-        GraphTraversalSource g = graph.traversal();
-        if (g.V().count().next() == 0) {
-            // load the schema and graph data
-            GraphOfTheGodsFactory.load(graph);
-        }
-        Vertex saturnProps = g.V().has("name", "saturn").next();
-        saturnProps.properties().forEachRemaining(prop -> System.out.println(prop.toString()));
-        List<Edge> places = g.E().has("place", Geo.geoWithin(Geoshape.circle(37.97, 23.72, 50))).toList();
-        System.out.println(places.toString());
-        System.exit(0);
+        JanusGraphVertex V1 = graph.addVertex();
+        V1.property("name", "V1");
+        JanusGraphVertex V2 = graph.addVertex();
+        V2.property("name", "V2");
+        JanusGraphVertex V3 = graph.addVertex();
+        V3.property("name", "V3");
+
+        JanusGraphVertex V4 = graph.addVertex();
+        V4.property("name", "V4");
+
+
+        V1.addEdge("simple edge",V2);
+        V2.addEdge("simple edge",V3);
+        V3.addEdge("simple edge",V4);
+
+        graph.tx().commit();
+
+        GraphTraversalSource G = graph.traversal();
+        Vertex v = G.V().has("name", "V1").next();
+
+        System.out.println(v.property("name").toString());
         graph.close();
     }
 }
